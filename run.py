bd3c42dafe854a9f17a94be6b76ff1681961a200
Simon Niklaus <simon.niklaus@outlook.com>
2021-03-01 08:54:58 -0800

    no message

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ torch.backends.cudnn.enabled = True # make sure to use cudnn for computational p
 
 ##########################################################
 
-arguments_strModel = 'default'
+arguments_strModel = 'default' # 'default', or 'chairs-things'
 arguments_strFirst = './images/first.png'
 arguments_strSecond = './images/second.png'
 arguments_strOut = './out.flo'
