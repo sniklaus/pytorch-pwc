c5cae3092d3477d4c22a7b9eaa8a60f1478308d8
Simon Niklaus <simon.niklaus@outlook.com>
2022-11-07 08:59:43 -0800

    no message

--- run.py
+++ run.py
@@ -16,8 +16,6 @@ except:
 
 ##########################################################
 
-assert(int(str('').join(torch.__version__.split('.')[0:2])) >= 13) # requires at least pytorch version 1.3.0
-
 torch.set_grad_enabled(False) # make sure to not compute gradients for computational performance
 
 torch.backends.cudnn.enabled = True # make sure to use cudnn for computational performance
