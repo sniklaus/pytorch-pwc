997f42d21912592463014dce56731b56c39abc64
Simon Niklaus <simon.niklaus@outlook.com>
2024-03-01 09:01:54 -0800

    no message

--- correlation/correlation.py
+++ correlation/correlation.py
@@ -270,7 +270,7 @@ def cupy_kernel(strFunction, objVariables):
 
 @cupy.memoize(for_each_device=True)
 def cupy_launch(strFunction, strKernel):
-    return cupy.cuda.compile_with_cache(strKernel).get_function(strFunction)
+    return cupy.RawKernel(strKernel, strFunction)
 # end
 
 class _FunctionCorrelation(torch.autograd.Function):
